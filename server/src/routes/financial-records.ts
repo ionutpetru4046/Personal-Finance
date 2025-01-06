33461e56b65fbe6ad2d6034f6bc297949747e045
Coste Ionut Petru <ionut4046@yahoo.com>
2025-01-06 19:05:19 +0000

    working on backend side

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -3,16 +3,29 @@ import FinancialRecordModel from "../schema/financial-record";
 
 const router = express.Router();
 
-router.get("/getAllByUserID/:userId", async (req: Request<{userId: string}>, res: Response) => {
+router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
     try {
         const userId = req.params.userId;
         const records = await FinancialRecordModel.find({ userId: userId });
         if (records.length === 0) {
-            return res.status(404).json({ message: "No records found for the user." });
+            return res.status(404).send({ message: "No records found for the user." });
         }
-        return res.status(200).json(records);
+        return res.status(200).send(records);
     } catch (err) {
-        return res.status(500).json({ error: "Internal server error" });
+        res.status(500).send(err);
+    }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId;
+        const records = await FinancialRecordModel.find({ userId: userId });
+        if (records.length === 0) {
+            return res.status(404).send({ message: "No records found for the user." });
+        }
+        return res.status(200).send(records);
+    } catch (err) {
+        res.status(500).send(err);
     }
 });
 
